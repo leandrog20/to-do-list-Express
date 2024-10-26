8229f7293c7fd815e50da3c8a7431824ab80dd39
Leandro gomes <107895852+leandrog20@users.noreply.github.com>
2024-10-26 17:49:14 -0300

    Middlewares

--- app.js
+++ app.js
@@ -3,9 +3,26 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas :)</h1>');
+    res.send('<h1>Minha lista de tarefas</h1>');
 })
 
+app.use(express.json());
+
+const log = (req, res, next) => {
+    console.log(req.body);
+    console.log(`Data: ${Date.now()}`);
+    next();
+}
+
+app.use(log)
+
+app.get("/json", (req, res) => {
+    res.json({
+        title: "Tarefa X",
+        done: true
+    });
+});
+
 app.listen(3000, () => {
-    console.log("Servidor iniciado")
+    console.log("Servidor iniciado");
 })
