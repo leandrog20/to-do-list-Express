effc2b0f20bece658046ef25572340e21a0fe67f
Leandro gomes <107895852+leandrog20@users.noreply.github.com>
2024-10-27 16:25:49 -0300

    Aula de Rotas

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
 const express = require("express");
-
+const checklistRouter = require('./src/routes/checklist')
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas</h1>');
-})
-
 app.use(express.json());
 
-const log = (req, res, next) => {
-    console.log(req.body);
-    console.log(`Data: ${Date.now()}`);
-    next();
-}
-
-app.use(log)
-
-app.get("/json", (req, res) => {
-    res.json({
-        title: "Tarefa X",
-        done: true
-    });
-});
+app.use("/checklists", checklistRouter)
 
 app.listen(3000, () => {
     console.log("Servidor iniciado");
---
+++ src/routes/checklist.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    console.log("Ola!")
+    res.send()
+})
+
+router.post("/", (req, res) => {
+    console.log(req.body)
+    res.status(200).json(req.body)
+})
+
+module.exports = router;
