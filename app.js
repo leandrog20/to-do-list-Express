315a82dc8ca66d0443c3214b054f065a664cdebd
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2024-10-30 15:58:53 -0300

    -

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const checklistRouter = require('./src/routes/checklist')
 const app = express();
 app.use(express.json());
 
-app.use("/checklists", checklistRouter)
+app.use("/checklists", checklistRouter);
 
 app.listen(3000, () => {
     console.log("Servidor iniciado");
