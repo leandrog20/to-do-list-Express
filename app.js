d1480ad9aea5296b849e3d5ea00a9e54a48a33ea
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2024-11-02 23:26:03 -0300

    Instalando o mongoose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const checklistRouter = require('./src/routes/checklist')
 const app = express();
+require("./config/database")
+
 app.use(express.json());
 
 app.use("/checklists", checklistRouter);
