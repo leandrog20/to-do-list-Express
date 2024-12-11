6cb4b73e50c4ea4f3f10df855899481a5eb0acda
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2024-12-11 18:18:38 -0300

    atualização

--- src/routes/task.js
+++ src/routes/task.js
@@ -14,6 +14,18 @@ checklistDependentRoute.get('/:id/tasks/new', async(req, res) => {
     }
 })
 
+simpleRouter.put('/:id', async(req, res) => {
+    let task = await Task.findById(req.params.id);
+    try{
+        task.set(req.body.task);
+        await task.save();
+        res.status(200).json({ task });
+    } catch(error) {
+        let errors = error.errors;
+        res.status(422).json({ task: {...errors }});
+    }
+})
+
 simpleRouter.delete('/:id', async(req, res) => {
     try{
         let task = await Task.findByIdAndDelete(req.params.id)
